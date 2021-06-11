b4080f5f0d05d9c6c66034dedec659b41670d85e
kinhnguyenngocpham3773604 <s3773604@student.rmit.edu.au>
2021-06-11 23:53:42 +1000

    a little test

--- src/main/test/BookingTest.java
+++ src/main/test/BookingTest.java
@@ -9,25 +9,31 @@ import java.sql.SQLException;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BookingTest {
+    public Booking book = new Booking();
 
     @Test
     void getBookColor() {
-        Assertions.assertEquals("-fx-background-color: GREEN", Booking.getBookColor("vacant"));
+        Assertions.assertEquals("-fx-background-color: GREEN", book.getBookColor("vacant"));
     }
 
     @Test
     void getBookState() throws SQLException {
-        assertEquals("vacant",Booking.getBookState(1));
+        //depend on the database
+        assertEquals("booked",book.getBookState(1));
     }
     @Test
-    void booking() {
+    void toBooking() throws SQLException {
+        //fail because there is no current user
+        assertFalse(book.toBooking("20","12","2021","12"));
     }
 
     @Test
-    void cancelBooking() {
+    void cancelBooking() throws SQLException {
+        assertTrue(book.cancelBooking("admin"));
     }
 
     @Test
-    void alreadyBooked() {
+    void alreadyBooked() throws SQLException {
+        assertFalse(book.alreadyBooked("admin"));
     }
 }
